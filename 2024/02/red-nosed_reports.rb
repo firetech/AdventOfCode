97e086aa3cf192d770b8c0b40dc7529e998cdb19
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-02 08:32:41 +0100

    2024/02 completed.

---
+++ 2024/02/red-nosed_reports.rb
@@ -0,0 +1,31 @@
+require_relative '../../lib/aoc'
+
+file = ARGV[0] || AOC.input_file()
+#file = 'example1'
+
+@levels = File.read(file).rstrip.split("\n").map { |l| l.split(/\s+/).map(&:to_i) }
+
+def is_safe(report)
+  diffs = report.each_cons(2).map { |a, b| a - b }
+  diffs.all? { |d| d > 0 and d <= 3 } or
+    diffs.all? { |d| d < 0 and d >= -3 }
+end
+
+# Part 1
+safe_reports = @levels.count { |report| is_safe(report) }
+puts "Safe reports: #{safe_reports}"
+
+# Part2
+safe_reports_damp = @levels.count do |report|
+  safe = is_safe(report)
+  if not safe
+    report.each_index do |i|
+      new_report = report.clone
+      new_report.delete_at(i)
+      safe = is_safe(new_report)
+      break if safe
+    end
+  end
+  safe
+end
+puts "Safe reports with dampener: #{safe_reports_damp}"
