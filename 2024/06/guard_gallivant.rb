422a5c36876e3ac25ab0885e465677dd5a8a2ffe
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-06 08:38:35 +0100

    2024/06 completed.
    Needs some optimizations and/or multicore enhancements, though.

--- 2024/06/guard_gallivant.rb
+++ 2024/06/guard_gallivant.rb
@@ -16,39 +16,65 @@ file = ARGV[0] || AOC.input_file()
 end
 raise 'No guard?!' if @start.nil?
 
-# Part 1
-dx, dy = [0, -1]
-x, y = @start
-@visited = Set[@start]
-inside = true
-while inside
-  new_x = nil
-  new_y = nil
-  begin
-    turning = false
-    new_y = y + dy
-    if new_y < 0 or new_y >= @map.length
-      inside = false
-      break
-    end
-    new_x = x + dx
-    if new_x < 0 or new_x >= @map[new_y].length
-      inside = false
-      break
-    end
-    # Turning right:
-    # 0, -1 => 1, 0
-    # 1, 0 => 0, 1
-    # 0, 1 => -1, 0
-    # -1, 0 => 0, -1
-    if @map[new_y][new_x] == '#'
-      turning = true
-      dx, dy = -dy, dx
-    end
-  end while turning
-  break unless inside
-  @visited << [new_x, new_y]
-  x = new_x
-  y = new_y
+DIR_TO_DXDY = [
+  # Turning right:
+  # 0, -1 => 1, 0
+  # 1, 0 => 0, 1
+  # 0, 1 => -1, 0
+  # -1, 0 => 0, -1
+  [0, -1],
+  [1, 0],
+  [0, 1],
+  [-1, 0]
+]
+
+def walk(obstruct_x = nil, obstruct_y = nil)
+  dir = 0
+  x, y = @start
+  visited = {}
+  inside = true
+  while inside
+    pos = [x, y]
+    visited[pos] ||= Set[]
+    return nil if visited[pos].include?(dir)
+    visited[pos] << dir
+
+    new_x = nil
+    new_y = nil
+    begin
+      dx, dy = DIR_TO_DXDY[dir]
+      turning = false
+      new_y = y + dy
+      if new_y < 0 or new_y >= @map.length
+        inside = false
+        break
+      end
+      new_x = x + dx
+      if new_x < 0 or new_x >= @map[new_y].length
+        inside = false
+        break
+      end
+      if @map[new_y][new_x] == '#' or
+          (new_x == obstruct_x and new_y == obstruct_y)
+        turning = true
+        dir = (dir + 1) % DIR_TO_DXDY.length
+      end
+    end while turning
+    x = new_x
+    y = new_y
+  end
+  return visited
 end
-puts @visited.size
+
+# Part 1
+@visited = walk
+puts "#{@visited.count} visited positions"
+
+# Part 2
+obstructions = 0
+@visited.each_key do |x, y|
+  if walk(x, y).nil?
+    obstructions += 1
+  end
+end
+puts "#{obstructions} possible obstructions cause a loop"
