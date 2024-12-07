1b43fd438b665b8d0aa423607ebfb9a66d35d868
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-07 13:19:41 +0100

    Speed up 2024/07.
    Do a DFS into the decision tree instead of trying every possible
    combination of operations individually.

--- 2024/07/bridge_repair.rb
+++ 2024/07/bridge_repair.rb
@@ -16,43 +16,54 @@ File.read(file).rstrip.split("\n").each do |line|
   end
 end
 
+PART1_OPERATORS = [:+, :*]
+PART2_OPERATORS = [:+, :*, :join]
+
+def is_possible(result, operands, operators)
+  # DFS into the decision tree
+  first_operand, *rest_operands = operands
+  stack = [[first_operand, rest_operands]]
+  until stack.empty?
+    value, operands_left = stack.pop
+
+    this_operand, *next_operands = operands_left
+
+    operators.each do |op|
+      if op == :join
+        new_value = "#{value}#{this_operand}".to_i
+      else
+        new_value = value.send(op, this_operand)
+      end
+      if next_operands.empty?
+        if new_value == result
+          return true
+        end
+      elsif new_value <= result
+        # All operations are increasing the resulting value, so intermediate
+        # values will never be > the result (as long as no operand is 0, which
+        # seems to be the case). It can, however be == result, since the rest
+        # of the operands may be 1.
+        stack << [new_value, next_operands]
+      end
+    end
+  end
+  return false
+end
 
 # Part 1
 true_sum = 0
 @equations.each do |result, operands|
-  [:+, :*].repeated_permutation(operands.count - 1) do |operators|
-    value = operands.first
-    operators.each_with_index do |op, i|
-      value = value.send(op, operands[i+1])
-      break if value > result
-    end
-    if value == result
-      true_sum += result
-      break
-    end
+  if is_possible(result, operands, PART1_OPERATORS)
+    true_sum += result
   end
 end
-
 puts "Total calibration result: #{true_sum}"
 
 # Part 2
 true_sum_concat = 0
 @equations.each do |result, operands|
-  [:+, :*, :join].repeated_permutation(operands.count - 1) do |operators|
-    value = operands.first
-    operators.each_with_index do |op, i|
-      if op == :join
-        value = "#{value}#{operands[i+1]}".to_i
-      else
-        value = value.send(op, operands[i+1])
-      end
-      break if value > result
-    end
-    if value == result
-      true_sum_concat += result
-      break
-    end
+  if is_possible(result, operands, PART2_OPERATORS)
+    true_sum_concat += result
   end
 end
-
 puts "Total calibration result with concatenation: #{true_sum_concat}"
