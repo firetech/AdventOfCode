ae4f32c51c5d547ae29297c7fbee0ce40342b9ce
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-09 15:06:59 +0100

    Minor cleanup of 2024/09.
    * Rename most central variables.
    * Return to file input (the real input is a bit long to be an argument).
    * Skip adding spaces with size 0 (minor optimization).

--- 2024/09/disk_fragmenter.rb
+++ 2024/09/disk_fragmenter.rb
@@ -1,61 +1,58 @@
 require_relative '../../lib/aoc'
 
-input = ARGV[0] || AOC.input()
-#input = '2333133121414131402'
+file = ARGV[0] || AOC.input_file()
+#file = 'example1'
 
-@map = input.strip.chars.map(&:to_i)
+@map = File.read(file).strip.chars.map(&:to_i)
 
-@part1_disk = []
-@part2_blocks = {}
-@part2_spaces = {}
+@disk = [] # Part 1
+@blocks = {} # Part 2
+@spaces = {} # Part 2
 id = 0
 @map.each_slice(2) do |file, free|
-  @part2_blocks[id] = [@part1_disk.length, file]
-  @part1_disk.push(*([id] * file))
-  unless free.nil?
-    @part2_spaces[@part1_disk.length] = free
-    @part1_disk.push(*([nil] * free))
+  @blocks[id] = [@disk.length, file]
+  @disk.push(*([id] * file))
+  if not free.nil? and free > 0
+    @spaces[@disk.length] = free
+    @disk.push(*([nil] * free))
   end
   id += 1
 end
 
-
-
 # Part 1
-@part1_disk.each_index do |i|
-  while i < @part1_disk.length and @part1_disk[i].nil?
-    new_data = @part1_disk.pop
-    @part1_disk[i] = new_data unless new_data.nil?
-    break if i == @part1_disk.length
+@disk.each_index do |i|
+  while @disk[i].nil?
+    new_data = @disk.pop
+    @disk[i] = new_data unless new_data.nil?
+    break if i == @disk.length
   end
 end
 checksum1 = 0
-@part1_disk.each_with_index { |x, i| checksum1 += x * i }
+@disk.each_with_index { |x, i| checksum1 += x * i }
 puts "Checksum after compaction: #{checksum1}"
 
 # Part 2
-@part2_blocks.each_key.reverse_each do |id|
-  i, length = @part2_blocks[id]
+@blocks.reverse_each do |id, (block_start, length)|
   space = nil
-  @part2_spaces.keys.sort.each do |start|
-    break if start >= i
-    if @part2_spaces[start] >= length
+  @spaces.keys.sort.each do |start|
+    break if start >= block_start
+    if @spaces[start] >= length
       space = start
       break
     end
   end
 
   unless space.nil?
-    @part2_blocks[id][0] = space
-    size = @part2_spaces.delete(space)
+    @blocks[id][0] = space
+    size = @spaces.delete(space)
     if size > length
-      @part2_spaces[space + length] = size - length
+      @spaces[space + length] = size - length
     end
   end
 end
 
 checksum2 = 0
-@part2_blocks.each do |id, (start, length)|
+@blocks.each do |id, (start, length)|
   length.times do |i|
     checksum2 += id * (start + i)
   end
