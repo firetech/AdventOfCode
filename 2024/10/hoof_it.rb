e3978ff5d0a85bbbafa6a8f7dcf90918bc706f6c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-10 13:25:00 +0100

    Minor cleanup of 2024/10.
    * Use bitmasks to represent positions.
    * Change map from a matrix to a Hash.
    * Don't keep track of height in DFS stack (it's available in the map).
    
    Doesn't really affect time, but looks nicer. Also reduces the checks
    needed while traversing the map (since outside the map will return nil,
    which is obviously != height + 1).

--- 2024/10/hoof_it.rb
+++ 2024/10/hoof_it.rb
@@ -4,26 +4,42 @@ require_relative '../../lib/aoc'
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
+X_BITS = 8
+X_MASK = (1 << X_BITS) - 1
+def to_pos(x, y)
+  return y << X_BITS | x
+end
+def from_pos(pos)
+  return pos & X_MASK, pos >> X_BITS
+end
+
 @heads = []
-@map = File.read(file).rstrip.split("\n").map.with_index do |line, y|
-  line.chars.map(&:to_i).each_with_index do |val, x|
-    if val == 0
-      @heads << [x, y]
+@map = {}
+File.read(file).rstrip.split("\n").each_with_index do |line, y|
+  line.chars.each_with_index do |val, x|
+    val_i = val.to_i
+    pos = to_pos(x, y)
+    @map[pos] = val_i
+    if val_i == 0
+      @heads << pos
     end
-  end # returns line.chars.map(&:to_i)
+  end
 end
 
 @peak_sum = 0 # Part 1
 @path_sum = 0 # Part 2
-@heads.each do |x, y|
-  stack = [[x, y, 0]]
+@heads.each do |head_pos|
+  stack = [head_pos]
   peaks = Set[]
   until stack.empty?
-    x, y, height = stack.pop
+    pos = stack.pop
+    height = @map[pos]
+
     if height == 9
-      @peak_sum += 1 if peaks.add?([x, y]) # Part 1
+      @peak_sum += 1 if peaks.add?(pos) # Part 1
       @path_sum += 1 # Part 2
     else
+      x, y = from_pos(pos)
       nheight = height + 1
       [
         [ 1,  0],
@@ -31,12 +47,9 @@ end
         [-1,  0],
         [ 0,  1]
       ].each do |dx, dy|
-        ny = y + dy
-        next if ny < 0 or @map[ny].nil?
-        nx = x + dx
-        next if nx < 0 or @map[ny][nx].nil?
-        next if @map[ny][nx] != nheight
-        stack << [nx, ny, nheight]
+        npos = to_pos(x + dx, y + dy)
+        next if @map[npos] != nheight
+        stack << npos
       end
     end
   end
