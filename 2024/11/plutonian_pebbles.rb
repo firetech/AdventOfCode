f5fad0b3c8e24ad8b8179b79405fc8a642101164
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-11 07:57:43 +0100

    2024/11 completed.

--- 2024/11/plutonian_pebbles.rb
+++ 2024/11/plutonian_pebbles.rb
@@ -3,28 +3,35 @@ require_relative '../../lib/aoc'
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
-@stones = File.read(file).rstrip.split(/\s+/).map(&:to_i)
+@counts = Hash.new(0)
+File.read(file).rstrip.split(/\s+/).each do |num|
+  @counts[num.to_i] += 1
+end
 
-def blink(stones)
-  new_stones = []
-  stones.each do |stone|
-    if stone == 0
-      new_stones << 1
+
+def blink(counts)
+  new_counts = Hash.new(0)
+  @counts.each do |num, count|
+    if num == 0
+      new_counts[1] += count
     else
-      digits = Math.log10(stone).floor + 1
+      digits = Math.log10(num).floor + 1
       if digits % 2 == 0
         div = 10**(digits/2)
-        new_stones << stone / div
-        new_stones << stone % div
+        new_counts[num / div] += count
+        new_counts[num % div] += count
       else
-        new_stones << stone * 2024
+        new_counts[num * 2024] += count
       end
     end
   end
-  return new_stones
+  return new_counts
 end
 
-stones = @stones
-25.times { stones = blink(stones) }
+# Part 1
+25.times { @counts = blink(@counts) }
+puts "Number of stones after 25 blinks: #{@counts.values.sum}"
 
-pp stones.count
+# Part 2
+50.times { @counts = blink(@counts) }
+puts "Number of stones after 75 blinks: #{@counts.values.sum}"
