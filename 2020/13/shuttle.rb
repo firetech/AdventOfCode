1bdca6b616ee5287bebda995e88c78435d27b81c
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-14 18:09:46 +0100

    Move Chinese Remainder Theorem implementation from 2020/13 to AOCMath.

--- 2020/13/shuttle.rb
+++ 2020/13/shuttle.rb
@@ -1,4 +1,5 @@
 require_relative '../../lib/aoc'
+require_relative '../../lib/aoc_math'
 
 file = ARGV[0] || AOC.input_file()
 #file = 'example1'
@@ -31,29 +32,6 @@ puts "Wait time: #{min_wait}, Line: #{best_line}, Score: #{min_wait * best_line}
 
 #part 2
 
-# Graciously stolen from https://rosettacode.org/wiki/Chinese_remainder_theorem#Ruby
-def extended_gcd(a, b)
-  last_remainder, remainder = a.abs, b.abs
-  x, last_x, y, last_y = 0, 1, 1, 0
-  while remainder != 0
-    last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
-    x, last_x = last_x - quotient*x, x
-    y, last_y = last_y - quotient*y, y
-  end
-  return last_remainder, last_x * (a < 0 ? -1 : 1)
-end
-def invmod(e, et)
-  g, x = extended_gcd(e, et)
-  if g != 1
-    raise 'Multiplicative inverse modulo does not exist!'
-  end
-  x % et
-end
-def chinese_remainder(mods, remainders)
-  max = mods.inject( :* )  # product of all moduli
-  series = remainders.zip(mods).map{ |r,m| (r * max * invmod(max/m, m) / m) }
-  series.inject( :+ ) % max
-end
 
 remainders = []
 lines.each_with_index do |line, i|
@@ -62,4 +40,4 @@ lines.each_with_index do |line, i|
   end
 end
 
-puts "Time of bus cascade: #{chinese_remainder(active_lines, remainders)}"
+puts "Time of bus cascade: #{AOCMath.chinese_remainder(active_lines, remainders)}"
