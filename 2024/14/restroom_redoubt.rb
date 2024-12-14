cf5a96a200d74335df6d19ce476213c4ade7a62a
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-14 19:02:50 +0100

    Speed up 2024/14 by applying math.
    Specifically, the Chinese Remainder Theorem.

--- 2024/14/restroom_redoubt.rb
+++ 2024/14/restroom_redoubt.rb
@@ -1,4 +1,5 @@
 require_relative '../../lib/aoc'
+require_relative '../../lib/aoc_math'
 
 file = ARGV[0] || AOC.input_file()
 @width = ARGV[1] || 101
@@ -23,49 +24,71 @@ end
 @w_split = @width/2
 @h_split = @height/2
 
-def get_quadrant(x, y)
-  if x < @w_split and y < @h_split
-    return 0
-  elsif x > @w_split and y < @h_split
-    return 1
-  elsif x < @w_split and y > @h_split
-    return 2
-  elsif x > @w_split and y > @h_split
-    return 3
-  end
-  return nil
-end
-
-
 # Part 1
-@quadrants = Array.new(4, 0)
+quadrants = Array.new(4, 0)
 @robots.each do |r|
-  q = get_quadrant(
-    (r[:x] + r[:dx] * 100) % @width,
-    (r[:y] + r[:dy] * 100) % @height
-  )
-  @quadrants[q] += 1 unless q.nil?
+  x = (r[:x] + r[:dx] * 100) % @width
+  y = (r[:y] + r[:dy] * 100) % @height
+  q = nil
+  if x < @w_split and y < @h_split
+    q = 0
+  elsif x > @w_split and y < @h_split
+    q = 1
+  elsif x < @w_split and y > @h_split
+    q = 2
+  elsif x > @w_split and y > @h_split
+    q = 3
+  else
+    next
+  end
+  quadrants[q] += 1
 end
-puts "Safety factor after 100 seconds: #{@quadrants.inject(&:*)}"
+puts "Safety factor after 100 seconds: #{quadrants.inject(&:*)}"
 
 # Part 2
-# Easiest way => find minimum safety factor
-cycles = 0
-min_factor = Float::INFINITY
-min_factor_cycles = nil
-(@width * @height).times do
-  cycles += 1
-  quadrants = Array.new(4, 0)
+# Find time with minimum variance in X and Y (individually)
+cycles = [@width, @height]
+min_vars = Array.new(2, Float::INFINITY)
+min_var_times = Array.new(2)
+cycles.max.times do |t|
+  positions = Array.new(2) { [] }
   @robots.each do |r|
-    r[:x] = (r[:x] + r[:dx]) % @width
-    r[:y] = (r[:y] + r[:dy]) % @height
-    q = get_quadrant(r[:x], r[:y])
-    quadrants[q] += 1 unless q.nil?
+    positions[0] << (r[:x] + r[:dx] * t) % @width
+    positions[1] << (r[:y] + r[:dy] * t) % @height
   end
-  factor = quadrants.inject(&:*)
-  if factor < min_factor
-    min_factor = factor
-    min_factor_cycles = cycles
+  positions.each_with_index do |p, i|
+    mean = p.sum.to_f / p.length
+    # Variance is actually this value divided by the length, but since we
+    # only want to find the minimum variance, that's an unnecessary step.
+    var_sum = p.sum { |x| (x - mean)**2 }
+    if var_sum < min_vars[i]
+      min_vars[i] = var_sum
+      min_var_times[i] = t
+    end
   end
 end
-puts "Easter egg seems to happen after #{min_factor_cycles} seconds"
+# The time when both of these happens at the same time can then be given by
+# inputting these values into the Chinese Remainder Theorem.
+min_var_time = AOCMath.chinese_remainder(cycles, min_var_times)
+puts "Easter egg seems to happen after #{min_var_time} seconds"
+
+# Uncomment to draw the easter egg
+=begin
+require 'set'
+r_pos = Set[]
+@robots.each do |r|
+  x = (r[:x] + r[:dx] * min_var_time) % @width
+  y = (r[:y] + r[:dy] * min_var_time) % @height
+  r_pos << [x, y]
+end
+@height.times do |y|
+  @width.times do |x|
+    if r_pos.include?([x, y])
+      print "\u2588"
+    else
+      print ' '
+    end
+  end
+  puts
+end
+=end
