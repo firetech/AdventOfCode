94654215904b5ed50b8e83f2b536462ed07c7cc6
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2024-12-25 21:18:07 +0100

    Speed up 2024/01.
    Count the occurences of each right value while parsing the input instead
    of when processing it in part 2.

--- 2024/01/historians_hysteria.rb
+++ 2024/01/historians_hysteria.rb
@@ -4,12 +4,15 @@ file = ARGV[0] || AOC.input_file()
 #file = 'example1'
 
 @left = []
-@right = []
+@right = [] # Part 1
+@rcount = Hash.new(0) # Part 2
 File.read(file).rstrip.split("\n").each do |line|
   case line
   when /\A(\d+)\s+(\d+)\z/
     @left << Regexp.last_match(1).to_i
-    @right << Regexp.last_match(2).to_i
+    right = Regexp.last_match(2).to_i
+    @right << right
+    @rcount[right] += 1
   else
     raise "Malformed line: '#{line}'"
   end
@@ -20,5 +23,5 @@ end
 puts "Total distance: #{@dist}"
 
 # Part 2
-@similarity = @left.sum { |x| x * @right.count(x) }
+@similarity = @left.sum { |x| x * @rcount[x] }
 puts "Similarity score: #{@similarity}"
